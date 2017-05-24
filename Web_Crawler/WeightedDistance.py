c9d0c72b583373232c8ce06da22b74cba9c798a0
wangdingkang <1257332448@qq.com>
2017-05-24 11:48:06 -0400

    some files update

--- Web_Crawler/WeightedDistance.py
+++ Web_Crawler/WeightedDistance.py
@@ -1,10 +1,10 @@
 import numpy as np
 
-alpha = 0.25
+alpha = 0.75
 
-input_file1 = 'distance_mat/L1/distances_dvec.txt'
-input_file2 = 'distance_mat/L1/distances_23478_removed.txt'
-output_file = 'distance_mat/L1/distances_dvec_L23478_' + str(alpha) + '_' + str(1 - alpha) + '.txt'
+input_file1 = 'distance_mat/L2/distances_dvec.txt'
+input_file2 = 'distance_mat/L2/distances_23478_removed.txt'
+output_file = 'distance_mat/L2/distances_dvec_L23478_' + str(alpha) + '_' + str(1 - alpha) + '.txt'
 
 if __name__ == '__main__':
     data1, data2 = [], []
